d1f8b355e0bad121247b37071f46cd8b7730a42f
LeAnhdung <jlehoola@yahoo.com>
2020-03-21 00:11:24 -0700

    converted for loop

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,8 +4,8 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] names = {"AA","BB", "CC", "DD"};
-		for (int i = 0; i <names.length; i++) {
-	System.out.format("Hello, %!\n", names[i]);
+		for (String name : names) {
+			System.out.format("Hello, %!\n", name);
 	}
 
 }
