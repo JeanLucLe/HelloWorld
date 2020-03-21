82ae264f6f0f0b6896d1d9442f6cb68ccc41cc41
LeAnhdung <jlehoola@yahoo.com>
2020-03-20 22:53:33 -0700

    m

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,10 +3,10 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello world! Branch2"); 
-		System.out.println("Hello world1!Branch2"); 
-		System.out.println("Hello world2!Branch2"); 
-		System.out.println("Hello world3!Branch2"); 
+		System.out.println("Hello world! M "); 
+		System.out.println("Hello world1!M1"); 
+		System.out.println("Hello world2!M2"); 
+		System.out.println("Hello world3!M2"); 
 
 	}
 
