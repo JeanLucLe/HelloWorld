5ab644c6a871fd79ef339e14e31d7f84aea4307e
LeAnhdung <jlehoola@yahoo.com>
2020-03-20 23:15:35 -0700

    m

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,10 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world! M "); 
-		System.out.println("Hello world1!M1"); 
-		System.out.println("Hello world2!M2"); 
-		System.out.println("Hello world3!M2"); 
-
+	
 	}
 
 }
